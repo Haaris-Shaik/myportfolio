0862a6b5c0913b02e4eeed3ad4431b44af851c34
Haaris <100534626+Haaris-Shaik@users.noreply.github.com>
2025-03-06 16:58:03 +0530

    Update Footer.tsx

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -7,10 +7,10 @@ function Footer() {
   return (
     <footer>
       <div>
-        <a href="https://github.com/yujisatojr" target="_blank" rel="noreferrer"><GitHubIcon/></a>
-        <a href="https://www.linkedin.com/in/yujisato/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
+        <a href="https://github.com/Haaris-Shaik/" target="_blank" rel="noreferrer"><GitHubIcon/></a>
+        <a href="https://www.linkedin.com/in/haaris-shaik/" target="_blank" rel="noreferrer"><LinkedInIcon/></a>
       </div>
-      <p>A portfolio designed & built by <a href="https://github.com/yujisatojr/react-portfolio-template" target="_blank" rel="noreferrer">Haaris</a> with 💜</p>
+      <p>A portfolio of <a href="https://www.linkedin.com/in/haaris-shaik/" target="_blank" rel="noreferrer">Haaris Shaik</a> with 💜</p>
     </footer>
   );
 }
